da3e35be94e4c70fc533a68a566e8e474cb5f5b8
Awa Diaby <awadiaby19@gmail.com>
2020-05-10 17:40:55 +0200

    Ceci est un jeu de mémoire

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,19 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import chai from 'chai'
+import createChaiEnzyme from 'chai-enzyme'
+import createChaiJestDiff from 'chai-jest-diff'
+import chaiJestSnapshot from 'chai-jest-snapshot'
+import enzymeToJSON from 'enzyme-to-json/serializer'
+import dirtyChai from 'dirty-chai'
+import { configure as configureEnzyme } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import sinonChai from 'sinon-chai'
+
+
+chai
+  .use(dirtyChai)
+  .use(createChaiJestDiff())
+  .use(chaiJestSnapshot)
+  .use(createChaiEnzyme())
+  .use(sinonChai)
+
+expect.addSnapshotSerializer(enzymeToJSON)
